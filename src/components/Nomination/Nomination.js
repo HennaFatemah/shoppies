c8038dcd366d39dad9b4e666d90df337c8a77e09
Henna Fatemah <henna_fatemah@yahoo.com>
2021-05-08 17:45:03 -0400

    nomination functionality and basic styling added

---
+++ src/components/Nomination/Nomination.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './Nomination.scss';
+import Poster from '../../assets/images/poster.jpeg';
+
+class Nomination extends Component {
+    render() {
+        const {nominatedMovies} = this.props;
+        return (
+            <>
+                <div className="nomination">
+                    {/* {nominatedMovies.length === 0 ? <div>No Nominations</div>: <div className="nomination__header">You have {nominatedMovies.length} Nominations</div>} */}
+                </div>
+                <div>
+                    <ul className="nomination__nominated-movies">
+                        {nominatedMovies.map((movie, i) => {
+                            return(
+                                <li key={i} className="nomination__nominated-list">
+                                    <div className="nomination__nominated-content">
+                                        <img className="nomination__nominated-pic" src={movie.Poster === "N/A" ? Poster: movie.Poster} alt={movie.Title}/>
+                                    </div>
+                                    <div>
+
+                                    <p className="nomination__nominated-title">{movie.Title}</p>
+                                    <button className="nomination__remove-btn" onClick={() => this.props.removeFromNomination(movie)}>Remove</button>
+                                    </div>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </>
+        );
+    }
+}
+
+export default Nomination;
