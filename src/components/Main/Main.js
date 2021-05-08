cb65e9f3474539d746549cf3fca6fde58eff3aac
Henna Fatemah <henna_fatemah@yahoo.com>
2021-05-08 12:43:35 -0400

    list of movies styled and functionality set

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
+import Movies from '../Movies/Movies';
 import './Main.scss'
 class Main extends Component {
+    constructor(){
+        super()
+        this.state = {
+            movies: [],
+            searchField: '',
+            sort: '',
+        }
+    }
+
+    onSearchChange = (e) => {
+        this.setState({ searchField: e.target.value })
+        fetch(`http://www.omdbapi.com/?apikey=874163ff&s=${this.state.searchField}`)
+        .then(response => response.json())
+        .then(searchedMovies => {
+            console.log(searchedMovies);
+            this.setState(searchedMovies.Response === "True" ? { movies: searchedMovies.Search }: null)
+        })
+    }
+
     render() {
         return (
             <div className="main-grid">
@@ -8,14 +28,18 @@ class Main extends Component {
                     <div className="main-grid__search">
                         <input 
                             type="search"
-                            placeholder="Search Movies"
+                            placeholder="Search Movies By Title ..."
                             className="main-grid__search-bar"
+                            onChange={this.onSearchChange}
                         />
                     </div>
                 </div>
                 <div className="main-grid__center">
                     <div className="main-grid__movies">
-                        <h2 className="main-grid__title">List of Movies</h2>
+                        <h2 className="main-grid__title">Results for "{this.state.searchField}"</h2>
+                        <Movies
+                            movies={this.state.movies}
+                        />
                     </div>
                     <div className="main-grid__nominations">
                         <h2 className="main-grid__title"><span  className="main-grid__title-count">0</span> Nominations</h2>
